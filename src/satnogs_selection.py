96856763b6f744b1aa537ba9074f404ea82df4ff
Jiaen Zhang <jiaenz@uci.edu>
2021-05-10 05:11:34 -0500

    change to list of dict

--- src/satnogs_selection.py
+++ src/satnogs_selection.py
@@ -1,4 +1,4 @@
-def satelliteFilter(satelliteList: list, mode="AFSK", baud=1200) -> [dict]:
+def satelliteFilter(satelliteList: [dict], mode: str = "AFSK", baud: int = 1200) -> [dict]:
     filtered = []
     for sat in satelliteList:
         if sat["mode"] == mode and sat["baud"] == int(baud):
@@ -8,7 +8,7 @@ def satelliteFilter(satelliteList: list, mode="AFSK", baud=1200) -> [dict]:
     return filtered
 
 
-def sortMostRecent(satelliteList: [dict], recent: object = True) -> object:
+def sortMostRecent(satelliteList: [dict], recent: bool = True) -> list:
     ret = sorted(satelliteList, key=lambda x: x["time"], reverse=recent)
 
     return ret
