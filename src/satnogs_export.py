56806358df0d1e204aa567239212622818831586
jiaenjez <83705797+jiaenjez@users.noreply.github.com>
2021-05-10 00:14:51 -0700

    Update satnogs_export.py

--- src/satnogs_export.py
+++ src/satnogs_export.py
@@ -1,38 +1,48 @@
-import requests  # json
-
-API_KEY = "36ff1b885dafa497e93073092cdac1c9887e510c"
-TRANSMITTER_URL = "https://db.satnogs.org/api/transmitters/?key=36ff1b885dafa497e93073092cdac1c9887e510c&format=json"
-SATELLITE_URL = "https://db.satnogs.org/api/satellites/?key=36ff1b885dafa497e93073092cdac1c9887e510c&format=json"
-TLE_URL = "https://db.satnogs.org/api/tle/?key=36ff1b885dafa497e93073092cdac1c9887e510c&format=json"
-TELEMETRY_URL = ""
-
-CELESTRAK_URL = "https://www.celestrak.com/satcat/tle.php?CATNR=46287"
+import satnogs_api
+import openpyxl  # excel
+from datetime import datetime
+import satnogs_selection
 
 
-def getID() -> set:
-    satellites = requests.get(SATELLITE_URL).json()
-    noradId = set()
-    for x in satellites:
-        noradId.add(x["norad_cat_id"])
+def export(satelliteList: [dict]) -> None:
+    if not satelliteList:  # if list is empty
+        return
 
-    return noradId
+    wb = openpyxl.Workbook()  # create a openpyxl WB object
+    ws = wb.create_sheet("Unfiltered", 0)  # raw output
+
+    ws["A1"] = "Name"
+    ws["B1"] = "Description"
+    ws["C1"] = "Norad_cat_id"
+    ws["D1"] = "Service"
+    ws["E1"] = "Mode"
+    ws["F1"] = "Baud Rate"
+    ws["G1"] = "Timestamp"
+
+    currRow = 2
+    for entry in satelliteList:
+        ws.cell(currRow, 1, entry["name"])
+        ws.cell(currRow, 2, entry["description"])
+        ws.cell(currRow, 3, entry["norad_cat_id"])
+        ws.cell(currRow, 4, entry["service"])
+        ws.cell(currRow, 5, entry["mode"])
+        ws.cell(currRow, 6, entry["baud"])
+        ws.cell(currRow, 7, entry["time"])
+        currRow += 1
+
+    ws = wb.create_sheet("Filtered", 1)  # filtered output
+    ws = wb.create_sheet("Sorted", 1)  # sorted output
+
+    # save to file
+    wb.save('D:\\satnogs_satellites.xlsx')
+
+    return
 
 
-def getSatellites() -> [dict]:
-    transmitters = requests.get(TRANSMITTER_URL).json()
-    satellites = requests.get(SATELLITE_URL).json()
-    sat = {}
-    for x in satellites:
-        sat[x["norad_cat_id"]] = x["name"]  # Get satellite name from DB
-    Satellites = []
-    for x in transmitters:  # Use Transmitter uuid to make looking up satellite from job easier
-        if x["alive"]:
-            Satellites.append({"name": sat[x["norad_cat_id"]], "description": x["description"],
-                               "norad_cat_id": x["norad_cat_id"],
-                               "service": x["service"], "mode": x["mode"],
-                               "baud": x["baud"], "time": x["updated"]})
-    return Satellites
-
-
-# def getTLE() -> dict:
-#     pass
+# driver
+satelliteList = satnogs_api.getSatellites()
+filter = satnogs_selection.satelliteFilter(satelliteList)
+sort = satnogs_selection.sortMostRecent(filter)
+export(sort)
+for v in sort:
+    print(v, end="\n")
