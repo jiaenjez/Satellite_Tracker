6cc190bea9b50a6124db0e16367e992730cb2623
Jiaen Zhang <jiaenz@uci.edu>
2021-05-17 13:16:42 -0500

    EarthSatellite Module can take two lines of string as a input, therefore TLE struct seems obsolete

--- src/tle.py
+++ src/tle.py
@@ -1,24 +1,24 @@
-#  helper functions
-def tleFormat(TLE: [dict]) -> [dict]:
-    pass
-    #  input: see excel TLE tab
-    #  output: [{key="Satellite", value="ISS"}, {key="Height", value="21130"}, {key="Velocity, value="..."} ...]
-    #  or some other formatting that makes processing easier
-
-
-# C-style struct
-class tle(object):
-    def __init__(self) -> None:
-        self.identifier = None
-        self.epoch = None
-        self.inclination = None
-        self.ascension = None
-        self.eccentricity = None
-        self.perigee = None
-        self.anomaly = None
-        self.motion = None
-
-
-    def __init_(self, raw: [dict]) -> None:
-        #  consider using tleFormat() here?
-        pass
+# #  helper functions
+# def tleFormat(TLE: [dict]) -> [dict]:
+#     pass
+#     #  input: see excel TLE tab
+#     #  output: [{key="Satellite", value="ISS"}, {key="Height", value="21130"}, {key="Velocity, value="..."} ...]
+#     #  or some other formatting that makes processing easier
+#
+#
+# # C-style struct
+# class tle(object):
+#     def __init__(self) -> None:
+#         self.identifier = None
+#         self.epoch = None
+#         self.inclination = None
+#         self.ascension = None
+#         self.eccentricity = None
+#         self.perigee = None
+#         self.anomaly = None
+#         self.motion = None
+#
+#
+#     def __init_(self, raw: [dict]) -> None:
+#         #  consider using tleFormat() here?
+#         pass
