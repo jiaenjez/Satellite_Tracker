af4c22fddd2fa0ef8911f927b242a7d19be7fe9b
Jiaen Zhang <jiaenz@uci.edu>
2021-05-20 11:23:17 -0500

    Time Zone Conversion using pytz.timezone

--- src/test.py
+++ src/test.py
@@ -1,7 +1,11 @@
+from matplotlib import pyplot
+
 from src import satnogs_api, satnogs_selection, satnogs_calc, location_input
 from skyfield.api import EarthSatellite, load
 import requests
 import pytz
+import numpy
+
 
 
 def testLocation() -> None:
@@ -46,18 +50,33 @@ def testGeneratePath(lines: [str, str, str]):
     satellite = EarthSatellite(lines[1], lines[2], lines[0], load.timescale())
     ts = load.timescale()
     xyz = []
+    x = []
+    y = []
+    z = []
     start = 0
-    end = 20
+    end = 120
 
     for minute in range(start, end):
         t = ts.utc(2021, 5, 22, 10, minute, 0)  # how can I specify time in a better way than doing this???
+        # need to build some kind of time array
         geocentric = satellite.at(t)
         xyz.append(geocentric.position.km)
+        x.append(geocentric.position.km[0])
+        y.append(geocentric.position.km[1])
+        z.append(geocentric.position.km[2])
 
     print("flightPath start time: ", ts.utc(2021, 5, 22, 10, start, 0).utc_strftime())
     print("flightPath end time: ", ts.utc(2021, 5, 22, 10, end - 1, 0).utc_strftime())
 
-    return xyz
+    print(x)
+    print(y)
+    print(z)
+
+    for l in xyz:
+        print(l)
+
+    pyplot.plot(x, y)
+    pyplot.show()
 
 
 def testTimeZoneConversion():
@@ -77,12 +96,45 @@ def testTimeZoneConversion():
     print("Curr Time in PST: ", pst)
 
 
-# driver
+def testPlot():
+    ts = load.timescale()
+    ts.utc(range(1900, 1950))  # Fifty years 1900–1949
+    ts.utc(1980, range(1, 25))  # 24 months of 1980 and 1981
+    ts.utc(2005, 5, [1, 11, 21])  # 1st, 11th, and 21st of May
+
+    # Negative values work too!  Here are the
+    # ten seconds crossing the 1974 leap second.
+    ts.utc(1975, 1, 1, 0, 0, range(-5, 5))
+
+    t = ts.utc(2020, 6, 16, 7, range(4))
+    for s in t.utc_strftime('%Y-%m-%d %H:%M'):
+        print(s)
+
+    planets = load('de421.bsp')
+    earth = planets['earth']
+
+    t = ts.utc(2014, 1, 1)
+    pos = earth.at(t).position.au
+    print(pos)
+
+    days = [1, 2, 3, 4]
+    t = ts.utc(2014, 1, days)
+    pos = earth.at(t).position.au
+    print(pos)
+
+    x, y, z = pos  # four values each
+    pyplot.plot(x, y)  # example matplotlib call
+    pyplot.show()
+
+
+
+"""
+driver
+"""
+
 response = testGetTLE()  # loading from API every time is slow, should load from a file instead
-xyz = testGeneratePath(response)
-for l in xyz:
-    print(l)
+testGeneratePath(response)
+
+# testPlot()
 
 # testTimeZoneConversion()
-
-
