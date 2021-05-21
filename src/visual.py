ec6373150c95ec0b4179e900d917d9d22611aea1
Jiaen Zhang <jiaenz@uci.edu>
2021-05-21 18:57:04 -0500

    added point of interest on plot

--- src/visual.py
+++ src/visual.py
@@ -8,7 +8,14 @@ def updatePath(lat: [float], long: [float], start: (float, float), timestamp) ->
     ax.annotate(f'▀█▀ {start[0]:.4f}, {start[1]:.4f} @ {timestamp.utc_strftime()}', start, color='red')
     ax.annotate(f'△ {"UC Irvine"}', (-117.841132, 33.643831), color='red')
     ax.annotate(f'△ {"CityLine, TX"}', (-96.697442, 32.999553), color='red')
-    ax.set(xlabel='longitude', ylabel='latitude')
+    ax.annotate(f'△ {"London, UK"}', (0.1278, 51.5074), color='red')
+    ax.annotate(f'△ {"Beijing, CHN"}', (116.4074, 39.9042), color='red')
+    ax.annotate(f'△ {"Singapore, CHN"}', (103.8198, 1.3521), color='red')
+    ax.annotate(f'△ {"Sydney, AUS"}', (-151.2093, -33.8688), color='red')
+    ax.annotate(f'△ {"Johannesburg, SA"}', (28.0473, -26.2041), color='red')
+    ax.annotate(f'△ {"Antarctica"}', (135, -82.8628), color='red')
+    ax.annotate(f'△ {"Antarctica"}', (-135, -82.8628), color='red')
+    ax.set(xlabel='longitude', ylabel='latitude', title='AMICALSAT')
     ax.grid()
 
 
@@ -25,7 +32,7 @@ def updateOrbit(x: [float], y: [float], z: [float], h: [float]) -> None:
     pyplot.grid()
 
 
-duration = 4 * 3600
+duration = 5 * 3600
 resolution = 1.0
 
 tle = satnogs_calc.loadTLE()
