f21fce03f3231e2fadd49eab89b77ec811dea7b4
Jiaen Zhang <jiaenz@uci.edu>
2021-05-31 08:58:02 -0500

    plot radio passes

--- src/visual.py
+++ src/visual.py
@@ -1,6 +1,11 @@
+import gc
+import random
+
 from matplotlib import pyplot, animation
 from matplotlib.animation import FuncAnimation
 from skyfield.api import load
+from skyfield.toposlib import wgs84
+
 from src import satnogs_calc, flightPath, satnogs_export
 import time
 
@@ -39,21 +44,23 @@ def singleFlightPath(lat: [float], long: [float], start: (float, float), timesta
         return annot,
 
     return animation.FuncAnimation(fig, update, frames=len(lat) - 1, init_func=init, interval=ANIMATION_SPEED,
-                                   blit=True)
+                                   blit=True, save_count=0)
 
 
-def getAllFlightPath():
+def getAllFlightPath(location: list, duration: float):
     file = satnogs_export.loadTLE(satnogs_export.TLE_DIR)
-    sats = []
-    count = 0
-    for r in file:
-        satellite = flightPath.flightPath(r['tle0'], r['tle1'], r['tle2'], 10.0 * 3600, 3)
-        print(count + 1, "/", len(file))
-        count += 1
-        satellite.findHorizonPath(satellite.findHorizonTime())
-        sats.append(satellite)
+    path = []
+    count = 1
 
-    return sats
+    for r in file:
+        satellite = flightPath.flightPath(r['tle0'], r['tle1'], r['tle2'], duration, 3)
+        for rx in location:
+            satellite.findHorizonPath(satellite.findHorizonTime(rx))
+        path.append(satellite)
+        print(f'{count} / {len(file)}')
+        count += 1
+
+    return path
 
 
 def plotAllFlightPath(allPath: []) -> FuncAnimation:
@@ -65,14 +72,10 @@ def plotAllFlightPath(allPath: []) -> FuncAnimation:
         ax.set_xlim([-180, 180])
         ax.set_ylim([-90, 90])
         ax.imshow(img, origin='upper', extent=[-180, 180, -90, 90], alpha=0.75)
-        ax.annotate(f'. {"UC Irvine"}', (-117.841132, 33.643831), color='black')
+        ax.annotate(f'. {"UCI"}', (-117.841132, 33.643831), color='black')
         ax.annotate(f'. {"Plano, TX"}', (-96.697442, 32.999553), color='black')
-        ax.annotate(f'. {"Anchorage, AK"}', (-149.9003, 61.2181), color='black')
-        ax.annotate(f'. {"NYC, NY"}', (-74.0060, 40.7128), color='black')
-        ax.annotate(f'. {"London, UK"}', (0.1278, 51.5074), color='black')
         ax.annotate(f'. {"Dalian, China"}', (121.6147, 38.9140), color='black')
         ax.annotate(f'. {"Singapore"}', (103.8198, 1.3521), color='black')
-        ax.annotate(f'. {"Johannesburg, South Africa"}', (28.0473, -26.2041), color='black')
         ax.set(xlabel='longitude', ylabel='latitude', title='NAME')
         # ax.grid()
 
@@ -82,20 +85,21 @@ def plotAllFlightPath(allPath: []) -> FuncAnimation:
         long = allPath[0].path[1]
         lat = allPath[0].path[0]
         currPath = ax.plot(long, lat, 'black', label='ground track', linewidth=2)
-        ax.legend(loc='upper right')
+        ax.legend(loc='lower right')
         return currPath,
 
     def update(frame):
         ax.cla()
+        gc.collect()
         setup()
         ax.set(xlabel='longitude', ylabel='latitude', title=allPath[frame + 1].name)
         long = allPath[frame + 1].path[1]
         lat = allPath[frame + 1].path[0]
         currPath = ax.plot(long, lat, 'black', label='ground track', linewidth=2)
-        ax.legend(loc='upper right')
+        ax.legend(loc='lower right')
         return currPath,
 
-    return animation.FuncAnimation(fig, update, frames=len(allPath) - 1, init_func=init, interval=1000)
+    return animation.FuncAnimation(fig, update, frames=len(allPath) - 1, init_func=init, interval=1000, save_count=0)
 
 
 def plotAllRadioPass(sats: []):
@@ -103,18 +107,20 @@ def plotAllRadioPass(sats: []):
     img = pyplot.imread("https://upload.wikimedia.org/wikipedia/commons/8/83/Equirectangular_projection_SW.jpg",
                         format='jpg')
 
+    def color():
+        red = random.random()
+        blue = random.random()
+        grey = random.random()
+        return red, grey, blue
+
     def setup():
         ax.set_xlim([-180, 180])
         ax.set_ylim([-90, 90])
         ax.imshow(img, origin='upper', extent=[-180, 180, -90, 90], alpha=0.75)
-        ax.annotate(f'. {"UC Irvine"}', (-117.841132, 33.643831), color='black')
+        ax.annotate(f'. {"UCI"}', (-117.841132, 33.643831), color='black')
         ax.annotate(f'. {"Plano, TX"}', (-96.697442, 32.999553), color='black')
-        ax.annotate(f'. {"Anchorage, AK"}', (-149.9003, 61.2181), color='black')
-        ax.annotate(f'. {"NYC, NY"}', (-74.0060, 40.7128), color='black')
-        ax.annotate(f'. {"London, UK"}', (0.1278, 51.5074), color='black')
         ax.annotate(f'. {"Dalian, China"}', (121.6147, 38.9140), color='black')
         ax.annotate(f'. {"Singapore"}', (103.8198, 1.3521), color='black')
-        ax.annotate(f'. {"Johannesburg, South Africa"}', (28.0473, -26.2041), color='black')
         ax.set(xlabel='longitude', ylabel='latitude', title='NAME')
         # ax.grid()
 
@@ -129,13 +135,14 @@ def plotAllRadioPass(sats: []):
             interval = p[3]
             t0 = interval[0].utc_strftime("%Y %b %d %H:%M:%S")
             t1 = interval[-1].utc_strftime("%H:%M:%S")
-            currPath = ax.plot(long, lat, 'black', label=f'{t0} - {t1}, Duration:{len(interval)}', linewidth=2)
+            currPath = ax.plot(long, lat, c=color(), label=f'{t0} - {t1}, Duration:{len(interval)}', linewidth=4)
         if sat.radioPass:
-            ax.legend(loc='upper right')
+            ax.legend(loc='lower right')
         return currPath,
 
     def update(frame):
         ax.cla()
+        gc.collect()
         setup()
         ax.set(xlabel='longitude', ylabel='latitude', title=sats[frame + 1].name)
         sat = sats[frame + 1]
@@ -146,12 +153,12 @@ def plotAllRadioPass(sats: []):
             interval = p[3]
             t0 = interval[0].utc_strftime("%Y %b %d %H:%M:%S")
             t1 = interval[-1].utc_strftime("%H:%M:%S")
-            currPath = ax.plot(long, lat, 'black', label=f'T: {t0} - {t1}, Duration:{len(interval)}', linewidth=2)
+            currPath = ax.plot(long, lat, c=color(), label=f'T: {t0} - {t1}, Duration:{len(interval)}', linewidth=4)
         if sat.radioPass:
-            ax.legend(loc='upper right')
+            ax.legend(loc='lower right')
         return currPath,
 
-    return animation.FuncAnimation(fig, update, frames=len(sats) - 1, init_func=init, interval=1000)
+    return animation.FuncAnimation(fig, update, frames=len(sats) - 1, init_func=init, interval=1000, save_count=0)
 
 
 # tle = satnogs_export.loadTLE()
@@ -160,8 +167,12 @@ def plotAllRadioPass(sats: []):
 # x, y, z, h = satnogs_calc.getOrbitPath(response, DURATION, RESOLUTION)
 # pyplot.show()
 
-s = getAllFlightPath()
+dalian = wgs84.latlon(38.9140, 121.6147, elevation_m=29)
+irvine = wgs84.latlon(33.643831, -117.841132, elevation_m=17)
+duration = 1 * 24 * 3600
+
+s = getAllFlightPath([dalian, irvine], duration)
 g = plotAllRadioPass(s)
-f = plotAllFlightPath(s)
-# f.save('D:\\path.gif', dpi=100)
+# f = plotAllFlightPath(s)
+# f.save('flightPath.gif', dpi=200)
 pyplot.show()
