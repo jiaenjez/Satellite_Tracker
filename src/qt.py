6458b08fb9e32a9ebd24eb6e216d3da0d02c63b1
Jiaen Zhang <jiaenz@uci.edu>
2021-06-06 05:04:43 -0500

    updated file

--- src/qt.py
+++ src/qt.py
@@ -1,22 +1,18 @@
+#  TODO https://www.mfitzp.com/tutorials/plotting-matplotlib/
 import sys
-import random
 import matplotlib
-
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from matplotlib import pyplot
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
 matplotlib.use('Qt5Agg')
 
 
-class MplCanvas(FigureCanvas):
+class MplCanvas(FigureCanvasQTAgg):
 
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        # fig = Figure(figsize=(width, height), dpi=dpi)
-        fig, self.ax = pyplot.subplots(figsize=(15, 7.5))
-        # self.ax = fig.add_subplot(111)
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
         super(MplCanvas, self).__init__(fig)
 
 
@@ -25,35 +21,22 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.canvas = MplCanvas(self, width=30, height=20, dpi=100)
-        self.setCentralWidget(self.canvas)
+        sc = MplCanvas(self, width=5, height=4, dpi=100)
+        sc.axes.plot([0,1,2,3,4], [10,1,20,3,40])
 
-        n_data = 50
-        self.xdata = list(range(n_data))
-        self.ydata = [random.randint(0, 10) for i in range(n_data)]
-        self.update_plot()
+        # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
+        toolbar = NavigationToolbar(sc, self)
 
-        button1 = QPushButton(self.canvas)
-        button1.setText("Button1")
-        button1.move(64, 16)
-        button1.clicked.connect(self.update_plot)
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(toolbar)
+        layout.addWidget(sc)
+
+        # Create a placeholder widget to hold our toolbar and canvas.
+        widget = QtWidgets.QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
         self.show()
-
-        # Setup a timer to trigger the redraw by calling update_plot.
-        # self.timer = QtCore.QTimer()
-        # self.timer.setInterval(100)
-        # self.timer.timeout.connect(self.update_plot)
-        # self.timer.start()
-
-
-    def update_plot(self):
-        # Drop off the first y element, append a new one.
-        self.ydata = self.ydata[1:] + [random.randint(0, 10)]
-        self.canvas.ax.cla()  # Clear the canvas.
-        self.canvas.ax.plot(self.xdata, self.ydata, 'r')
-        # Trigger the canvas to update and redraw.
-        self.canvas.draw()
 
 
 app = QtWidgets.QApplication(sys.argv)
